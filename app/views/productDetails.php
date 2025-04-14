07eb71d500fec73be25a785e065bd7d183f9d650
Samad126 <samed192005@gmail.com>
2025-04-14 19:15:07 +0400

    added product details

---
+++ app/views/productDetails.php
@@ -0,0 +1,31 @@
+<!-- BREADCRUMB -->
+<?php include_once 'partials/productDetails/breadcrumb.php'; ?>
+<!-- /BREADCRUMB -->
+
+<!-- Main Part -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- Photo Gallery -->
+            <?php include_once __DIR__ . '/partials/productDetails/photoGallery.php'; ?>
+            <!-- /Photo Gallery -->
+
+            <!-- Product details -->
+            <?php include_once __DIR__ . '/partials/productDetails/productDetails.php'; ?>
+            <!-- /Product details -->
+
+            <!-- Product tab -->
+            <?php include_once __DIR__ . '/partials/productDetails/extraDetailsReview.php'; ?>
+            <!-- /product tab -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /Main Part -->
+
+<!-- Related -->
+<?php include_once 'partials/productDetails/relatedProduct.php'; ?>
+<!-- /Related -->
