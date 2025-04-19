78296ffa69a10747fffbe23c7f8344404dff37d3
Samad126 <samed192005@gmail.com>
2025-04-19 22:10:12 +0400

    Add Composer dependencies and autoload files
    - Generated autoload files for Composer.
    - Added installed.json and installed.php to track package installations.
    - Included platform_check.php to enforce PHP version requirements.
    - Updated dependencies including Cloudinary SDK, Guzzle, Monolog, and PSR packages.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc361c73eab5b8000910da3831adc0391
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc361c73eab5b8000910da3831adc0391', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc361c73eab5b8000910da3831adc0391', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitc361c73eab5b8000910da3831adc0391
         call_user_func(\Composer\Autoload\ComposerStaticInitc361c73eab5b8000910da3831adc0391::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc361c73eab5b8000910da3831adc0391::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
