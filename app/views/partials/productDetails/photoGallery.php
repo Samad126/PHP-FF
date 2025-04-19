996d65b02f46e898ac96a31c7ade987db2214270
Samad126 <samed192005@gmail.com>
2025-04-19 22:37:56 +0400

    feat: enhance product image display with dynamic loading and improved CSS styles

--- app/views/partials/productDetails/photoGallery.php
+++ app/views/partials/productDetails/photoGallery.php
@@ -1,41 +1,21 @@
+<!-- Product main img -->
 <div class="col-md-5 col-md-push-2">
     <div id="product-main-img">
+        <?php foreach($product->images as $image): ?>
         <div class="product-preview">
-            <img src="/assets/img/product01.png" alt="">
+            <img src="<?= htmlspecialchars($image) ?>" alt="<?= htmlspecialchars($product->name) ?>">
         </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product03.png" alt="">
-        </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product06.png" alt="">
-        </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product08.png" alt="">
-        </div>
+        <?php endforeach; ?>
     </div>
 </div>
-<!-- /Product main img -->
 
 <!-- Product thumb imgs -->
-<div class="col-md-2  col-md-pull-5">
+<div class="col-md-2 col-md-pull-5">
     <div id="product-imgs">
+        <?php foreach($product->images as $image): ?>
         <div class="product-preview">
-            <img src="/assets/img/product01.png" alt="">
+            <img src="<?= htmlspecialchars($image) ?>" alt="<?= htmlspecialchars($product->name) ?>">
         </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product03.png" alt="">
-        </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product06.png" alt="">
-        </div>
-
-        <div class="product-preview">
-            <img src="/assets/img/product08.png" alt="">
-        </div>
+        <?php endforeach; ?>
     </div>
 </div>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -19,7 +19,9 @@
 		var $this = $(this),
 				$nav = $this.attr('data-nav');
 
-		$this.slick({
+		$this.on('init', function(event, slick) {
+			$(this).css({opacity: 1, visibility: 'visible'});
+		}).slick({
 			slidesToShow: 4,
 			slidesToScroll: 1,
 			autoplay: true,
@@ -64,35 +66,38 @@
 	/////////////////////////////////////////
 
 	// Product Main img Slick
-	$('#product-main-img').slick({
-    infinite: true,
-    speed: 300,
-    dots: false,
-    arrows: true,
-    fade: true,
-    asNavFor: '#product-imgs',
-  });
+	$('#product-main-img').on('init', function(event, slick) {
+		$(this).css({opacity: 1, visibility: 'visible'});
+	}).slick({
+		infinfite: true,
+		speed: 300,
+		dots: false,
+		arrows: true,
+		fade: true,
+		asNavFor: '#product-imgs',
+	});
 
 	// Product imgs Slick
-  $('#product-imgs').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: true,
-    focusOnSelect: true,
+	$('#product-imgs').on('init', function(event, slick) {
+		$(this).css({opacity: 1, visibility: 'visible'});
+	}).slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		focusOnSelect: true,
 		centerPadding: 0,
 		vertical: true,
-    asNavFor: '#product-main-img',
+		asNavFor: '#product-main-img',
 		responsive: [{
-        breakpoint: 991,
-        settings: {
-					vertical: false,
-					arrows: false,
-					dots: true,
-        }
-      },
-    ]
-  });
+			breakpoint: 991,
+			settings: {
+				vertical: false,
+				arrows: false,
+				dots: true,
+			}
+		}]
+	});
 
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
